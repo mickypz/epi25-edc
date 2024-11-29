7f3e64285956a3190dec38b426205138d3091f88
Milena Zizovic <milena.zizovic@uni.lu>
2024-11-29 15:08:34 +0100

    deleted

--- code/config.R
+++
@@ -1,7 +0,0 @@
-# config file
-library(REDCapR)
-library(redcapAPI)
-suppressPackageStartupMessages(library(here))
-suppressPackageStartupMessages(library(tidyverse))
-
-source("api-tokens.R")
--- code/load-dict.R
+++
@@ -1,9 +0,0 @@
-# Load data dictionaries
-dict_col_names <- c("field_name", "form_name", "section_header", "field_type", "field_label", "select_choices_or_calculations", "field_note", "text_validation_type_or_show_slider_number", "text_validation_min", "text_validation_max", "identifier", "branching_logic", "required_field", "custom_alignment", "question_number", "matrix_group_name", "matrix_ranking", "field_annotation")
-
-fp <- here("DataDictionaries")
-
-# Focal
-dict_focal <-read_csv(file = here(fp,  "Epi25Focal.csv"), 
-                      col_types = c("ccccccccddcccciccc"))|> 
-  set_names(dict_col_names)
